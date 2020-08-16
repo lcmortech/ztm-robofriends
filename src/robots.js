2a39a1cd357f47bb1c39c6963886e26e30e47054
rg326 <50557156+rg326@users.noreply.github.com>
2020-08-16 15:04:30 -0400

    adding robofriends repo

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+
+{
+id: 1,
+name: 'Leanne Graham',
+username: 'Lea',
+email: 'Sincere@april.biz'
+},
+{
+id: 2,
+name: 'Ryan Briggs',
+username: 'Rye',
+email: 'ustagauy@pix.net'
+},
+{
+id: 3,
+    name: 'Kacey Ramsay',
+username: 'Kay',
+    email: 'multispi@subiri.gq'
+},
+{
+id: 4,
+    name: 'Daisy-Mae Huffman',
+username: 'Daz',
+    email: 'kobibo3467@bewedfv.com'
+},
+{
+id: 5,
+    name: 'Ikrah Rodgers',
+username: 'Ike',
+email: 'Sincere@april.biz'
+},
+{
+id: 6,
+    name: 'Fynn Woodward',
+username: 'Fin',
+email: 'Sincere@april.biz'
+},
+{
+id: 7,
+    name: 'Khushi Horn',
+username: 'Kush',
+email: 'Sincere@april.biz'
+},
+{
+id: 8,
+    name: 'Kairo Johns',
+username: 'Kai',
+email: 'Sincere@april.biz'
+},
+{
+id: 9,
+    name: 'Danny Miranda',
+username: 'Dan',
+email: 'Sincere@april.biz'
+},
+{
+id: 10,
+    name: 'Cyrus Garrison',
+username: 'Cy',
+email: 'Sincere@april.biz'
+}
+
+
+]
